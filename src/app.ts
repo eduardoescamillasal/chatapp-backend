b98550aa864811612ff980115d54a24fbc156b50
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-01-21 09:30:05 -0600

    database connection setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 
 import { ChatAppServer } from "./setupServer";
+import databaseConnection from "./setupDatabase";
 
 class Application {
   public initialize(): void {
+    databaseConnection();
     const app: Express = express();
     const server: ChatAppServer = new ChatAppServer(app);
     server.start();
