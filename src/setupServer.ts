35d6c50fe7994d25f2d8df0ab7187da47360ab94
Eduardo Escamilla Saldaña <eduardo.escamillasal@gmail.com>
2025-01-22 18:54:32 -0600

    global error handler

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -19,6 +19,10 @@ import { createAdapter } from "@socket.io/redis-adapter";
 import "express-async-errors";
 import { config } from "./config";
 import applicationRoutes from "./routes";
+import {
+  CustomError,
+  IErrorResponse,
+} from "./shared/globals/helpers/error-handler";
 
 const SERVER_PORT = 5009;
 
@@ -68,7 +72,28 @@ export class ChatAppServer {
     applicationRoutes(app);
   }
 
-  private globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {
+    app.all("*", (req: Request, res: Response) => {
+      res
+        .status(HTTP_STATUS.NOT_FOUND)
+        .json({ message: `${req.originalUrl} not found` });
+    });
+
+    app.use(
+      (
+        error: IErrorResponse,
+        _req: Request,
+        res: Response,
+        next: NextFunction
+      ): any => {
+        console.log(error);
+        if (error instanceof CustomError) {
+          return res.status(error.statusCode).json(error.serializeErrors());
+        }
+        next();
+      }
+    );
+  }
 
   private async startServer(app: Application): Promise<void> {
     try {
